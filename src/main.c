808d0014f9b3aa2fb6bf93b2cc8a541a84def30a
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-07-22 14:30:13 +0100

    Aded GTK4 support for modern GUI

---
+++ src/main.c
@@ -0,0 +1,31 @@
+#include <gtk/gtk.h>
+#include "gui/main_window.h"
+#include "database.h"
+#include "fingerprint.h"
+#include <gtk/gtktypes.h>
+
+
+
+int main(int argc, char *argv[]) {
+    gtk_init(&argc, &argv);
+
+    if (!initialize_database()){
+        g_printerr("Failed to initialize database\n");
+        return 1;
+    }
+
+    if (!initialize_fingerprint_sensor()){
+        g_printer("Failed to initialize fingerprint sensor\n");
+        return 1;
+    }
+
+    GtkWidget *window = create_main_window();
+    gtk_widget_show(window);
+
+    gtk_main();
+
+    close_database();
+    cleanup_fingerprint_sensor();
+
+    return 0;
+}
